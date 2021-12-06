702fe27b485cd2125a34e672c586c3df1d80edce
BryanGlennardy <bryanglennardy24@gmail.com>
2021-12-07 01:17:20 +0700

    BryanGlennardy-SplashScreen

---
+++ app/src/main/java/id/ac/umn/walls/SplashActivity.java
@@ -0,0 +1,29 @@
+package id.ac.umn.walls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L);
+
+    }
+}
